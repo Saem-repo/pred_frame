9bc93ec30f7753af8794921d8887f4472ebb64a9
saem <saem@kaist.ac.kr>
2022-05-01 00:46:19 +0900

    fourth commit

--- app.py
+++ app.py
@@ -510,7 +510,7 @@ elif menu == "Development of Prediction Model" :
     #     ("Building Energy", "Individual Thermal Comfort","Natural Ventilation Rate"),
     # )
     
-    st.markdown("""
+    intro = """
                 Here we perform development of each predictive model according to building tasks based on transfer learning with different ensembled strategies.
                 
                 ### Steps for development of predictive model 
@@ -523,10 +523,13 @@ elif menu == "Development of Prediction Model" :
                 * **Caution :** 
                     - Files should be in the form of CSV or XLSX (Excel file)
                     - Variables used for development of predictive model in both Source and Target should be identical. (If not, Cannot operate!!)
-                    ![data_order](./img/Data_Order.png)
+                    ![image](img/Data_Order.png)
                 4. Choose the ratio of fine-tuned data in target domain and the number of ensembled networks (If using ETL or HETL)
                 5. Push the button "Build Predictive Model"   
-                """)
+                """
+    
+    st.markdown(intro)
+    
     
     st.markdown("---")
     
